c53a4b5173559647f2920a99f9b5837bbb5a63d0
Mangel <mangelarthur1127@gmail.com>
2025-03-20 14:31:04 +0100

    noeud trefle et noeud simple

--- type_noeuds/knot.c
+++ type_noeuds/knot.c
@@ -14,9 +14,12 @@ struct rope
 typedef  struct rope rope;
 typedef  struct rope knot;
 
+//constructeur
+
 knot *knot_create(knot *c_l,knot *x_l,knot *c_r,knot *x_r)
 {
   knot *k=malloc(sizeof(knot));
+
   k-> cut_l=c_l;
   k-> cross_l=x_l;
   k-> cut_r=c_r;
@@ -24,6 +27,43 @@ knot *knot_create(knot *c_l,knot *x_l,knot *c_r,knot *x_r)
 
   return k;
 }
+
+knot *trivial_knot_create()
+{
+  knot *k=malloc(sizeof(knot));
+
+  k-> cut_l=k;
+  k-> cross_l=NULL;
+  k-> cut_r=k;
+  k-> cross_r=NULL;
+
+  return k;
+}
+
+knot *clover_knot_create()
+{
+  knot *k1=malloc(sizeof(knot));
+  knot *k2=malloc(sizeof(knot));
+  knot *k3=malloc(sizeof(knot));
+
+  k1-> cut_l=k3;
+  k1-> cross_l=k2;
+  k1-> cut_r=k2;
+  k1-> cross_r=k3;
+
+  k2-> cut_l=k1;
+  k2-> cross_l=k3;
+  k2-> cut_r=k3;
+  k2-> cross_r=k1;
+
+  k3-> cut_l=k2;
+  k3-> cross_l=k1;
+  k3-> cut_r=k1;
+  k3-> cross_r=k2;
+
+  return k1;
+}
+
 
 int main()
 {
