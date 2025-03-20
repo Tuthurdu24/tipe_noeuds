3cde9e70b8008f9c7cb53a7ac4ec22f3267ceaf5
Jeremy <jay@jerhern77@gmail.com>
2025-03-20 19:04:11 +0100

    clover_knot_rapide ajoutée

--- type_noeuds/knot.c
+++ type_noeuds/knot.c
@@ -31,7 +31,7 @@ knot *clover_knot_create()
   knot *k1=malloc(sizeof(knot));
   knot *k2=malloc(sizeof(knot));
   knot *k3=malloc(sizeof(knot));
-
+  
   k1-> cut_p=k3;
   k1-> x_p=k2;
   k1-> cut_n=k2;
@@ -45,9 +45,20 @@ knot *clover_knot_create()
   k3-> cut_p=k2;
   k3-> x_p=k1;
   k3-> cut_n=k1;
-  k3-> x_n=k2;
+  k3-> x_n=k2
 
   return k1;
+}
+
+knot* clover_knot_create_rapide()
+{
+  knot *k1=malloc(sizeof(knot));
+  knot *k2=malloc(sizeof(knot));
+  knot *k3=malloc(sizeof(knot));
+  
+  knot_tie(k1, k2, k3);
+  knot_tie(k2, k3, k1);
+  knot_tie(k3, k1, k2);
 }
 
 void knot_tie(knot* kp, knot* kn, knot* k_up)       // "attache" les cordes kp (knot_previous) et kn (knot_next) et fait passer la corde k_up au dessus de l'intersection.
